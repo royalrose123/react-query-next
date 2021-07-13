93eaac4557201b1b9c6c5ce00c414e12f1a81bf9
TUTORABC\royalchen <royalchen@gmail.com>
2021-07-13 10:57:32 +0800

    Update base path by next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const withImages = require('next-images')
 
 module.exports = withImages({
+  basePath: '/web/marketing',
   sassOptions: {
     additionalData: '@import "./src/styles/_variables.scss";@import "./src/styles/mixins/mixins.scss";',
   },
